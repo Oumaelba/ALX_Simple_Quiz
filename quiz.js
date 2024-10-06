918bbd79b26e0f9a86c7ca57e219abebeb3bbb2a
Oumaelba <elbachirioumaima16@gmail.com>
2024-10-06 18:08:13 +0100

    0. Implement a Simple Interactive Quiz 4th try

--- quiz.js
+++ quiz.js
@@ -1,9 +1,9 @@
 //0. Implement a Simple Interactive Quiz:
 function checkAnswer(){
 	const correctAnswer = "4";
-	const userAnswer = document.querySelector("input[name='quiz']:checked");
-	if (userAnswer) {
-		const userAnswer = userAnswer.value;
+	const userAnswerElement = document.querySelector("input[name='quiz']:checked");
+	if (userAnswerElement) {
+		const userAnswer = userAnswerElement.value;
         if (userAnswer === correctAnswer) {
             document.getElementById("feedback").textContent = "Correct! Well done.";
         } else {
