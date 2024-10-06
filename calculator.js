54429f41ed9eb7b4c11dc46e3dc9c4db679bbb5d
Oumaelba <elbachirioumaima16@gmail.com>
2024-10-06 18:55:19 +0100

    1. Build a Simple Calculator

---
+++ calculator.js
@@ -0,0 +1,37 @@
+// 1. Build a Simple Calculator:
+function add(number1, number2){
+	return number1 + number2;
+}
+function sub(number1, number2){
+	return number1 - number2;
+}
+function mul(number1, number2){
+	return number1 * number2;
+}
+function div(number1, number2){
+	return number1 / number2;
+}
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('subtract').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = sub(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('multiply').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = mul(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('divide').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = div(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
